465cacf860f5b1129e4fe63ce85daab38c568c38
jloperad <51926858+jloperad@users.noreply.github.com>
2022-06-27 19:20:05 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -68,7 +68,7 @@ def pregunta_01():
 
     # Remueva la columna `veil-type` del DataFrame `df`.
     # Esta columna tiene un valor constante y no sirve para la detección de hongos.
-    df.drop(columns=["veil_type"], inplace=True)
+    df.drop(columns=["veil-type"], inplace=True)
 
     # Asigne la columna `type` a la variable `y`.
     y = df["type"]
